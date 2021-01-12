48fbb1f40ba0c6516ab074ead5b610c196248a6f
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-12 16:25:05 +0100

    prova

---
+++ main.js
@@ -0,0 +1,13 @@
+// $('.close').click(function() {
+//     $('.hamburger-menu').hide();
+//   })
+
+
+  $(document).ready(function(){
+    $('.close').click(function() {
+        $('.hamburger-menu').show();
+      })
+      $('.close').click(function() {
+        $('.hamburger-menu').hide();
+      })
+  });
